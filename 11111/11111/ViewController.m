db36dcc9748d119a9a08766cf25979218cb2f005
genie <genie@geniedeMacBook-Pro-2.local>
2016-05-04 21:50:58 +0800

    log

--- 11111/11111/ViewController.m
+++ 11111/11111/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"hhjjhhkh");
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"11111111");
 }
